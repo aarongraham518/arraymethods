e07c805ce469694ad2730d1e01e6f20d660a437f
Aaron <aarongraham518@gmail.com>
2020-09-03 20:21:35 -0400

    create companies array of objects

---
+++ main.js
@@ -0,0 +1,11 @@
+const companies = [
+    {name: "Company one", category: "Finance", start: 1981, end: 2003},
+    {name: "Company two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company nine", category: "Retail", start: 1981, end: 1989}
+]
