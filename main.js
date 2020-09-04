94a535e379c285d167b7e8f9ed22e7c506b67704
Aaron <aarongraham518@gmail.com>
2020-09-03 20:30:53 -0400

    working with forEach Method

--- main.js
+++ main.js
@@ -8,4 +8,39 @@ const companies = [
     {name: "Company seven", category: "Auto", start: 1986, end: 1996},
     {name: "Company eight", category: "Technology", start: 2011, end: 2016},
     {name: "Company nine", category: "Retail", start: 1981, end: 1989}
-]
+];
+
+const ages = [33,12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//old way
+// for(let i = 0; i < companies.length; i++){
+//     console.log(companies[i]);
+// }
+
+//forEach can take in...(company, index ,companies)
+// companies.forEach(function(company){
+//     console.log(company.name);
+// });
+
+//filter
+/* all of the ages over 21 years old*/
+//old way
+// let canDrink = [];
+// for(let i=0; i < ages.length; i++){
+//     if(ages[i] >= 21){
+//         canDrink.push(ages[i]);
+//     }
+// }
+
+// console.log(canDrink);
+
+const canDrink = ages.filter(function(age){
+    
+});
+
+//map
+
+//sort
+
+//reduce
+
